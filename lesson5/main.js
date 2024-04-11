bf9f2fdbc65a23d0fb47d9e616d3fc09d2afde16
shshsheep <tung.lung38@gmail.com>
2024-04-11 21:45:55 +0800

    if val === +
    update the  new function

--- lesson5/main.js
+++ lesson5/main.js
@@ -1,5 +1,11 @@
 let val;
-let expression="";
+let expression = " ";
+let operatorsClicked = {
+    '+' : false,
+    '-' : false,
+    '*' : false,
+    '/' : false
+};
 
 function init(){
     let btns = document.querySelectorAll("button")
@@ -11,20 +17,36 @@ function init(){
         val = this.value;
         let display = document.getElementById("display");
 
+        if(val === '+' || val === '-' || val === '*' || val === '/'){
+            if(operatorsClicked[val]){
+                return;
+            }else {
+                operatorsClicked[val] = true;
+            }
+        }
+
         switch(val){
             case '=':
                 let result = eval(expression);
                 display.innerHTML = result;
                 expression = " ";
+                resoperators();
                 break;
             case 'C':
                 display.innerHTML = "0";
                 expression = " ";
+                resoperators();
                 break;
             default:
                 expression += val;
                 display.innerHTML = expression;
                 break;
+        }
+    }
+
+    function resoperators(){
+        for (let op in operatorsClicked){
+            operatorsClicked = false;
         }
     }
 }
