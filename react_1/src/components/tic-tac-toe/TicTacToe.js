6cef4686709381ab5cb72c3cdf1e70328449a359
shshsheep <tung.lung38@gmail.com>
2024-05-30 17:42:31 +0800

    caculateWinner

--- react_1/src/components/tic-tac-toe/TicTacToe.js
+++ react_1/src/components/tic-tac-toe/TicTacToe.js
@@ -2,15 +2,25 @@ import React, {useState} from "react";
 import Board from "./board";
 
 function TicTacToe(){
-    const [history, setHistory] = useState([Array(9).fill('A')]);
+    const [history, setHistory] = useState([Array(9).fill(null)]);
     const [currentMove,setcurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
     const currentSquares = history[currentMove];
-    return(
+
+    const handleplay = (nextSquares) =>{
+        const nextHistory = [...history.slice(0, currentMove+1), nextSquares]
+        setHistory(nextHistory);
+        setcurrentMove(nextHistory.length - 1);
+    }
+
+    return( 
         <div className="Game">
             <div className="GameBoard">
-                <Board squares={currentSquares} />
+                <Board xIsNext={xIsNext} squares={currentSquares} onplay={handleplay}/>
             </div>
-
+            <div className='game-info'>
+                <h6>遊戲歷程</h6>
+            </div>
         </div>
     )
 }
--- react_1/src/components/tic-tac-toe/board.js
+++ react_1/src/components/tic-tac-toe/board.js
@@ -2,12 +2,52 @@ import React from "react";
 import Square from "./square";
 import './tictactoe.css';
 
-function Board({squares}){
+function Board({xIsNext,squares, onplay}){
     const handleClick = (i) =>{
+        if (calculateWinner (squares || squares[i])){
+            return;
+        };
+        const nextSquares = squares.slice();
+        nextSquares[i] = xIsNext ? "X" : "O" ;
+        onplay(nextSquares);
+    };
+    
+    const calculateWinner = (squares) => {
+        const lines = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (
+              squares[a] &&
+              squares[a] === squares[b] &&
+              squares[a] === squares[c]
+            ) {
+              return squares[a];
+            }
+          }
 
+          return null;
+    };
+
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+      status = "贏家: " + winner;
+    } else {
+      status = "下一個玩家： " + (xIsNext ? "X" : "O");
     }
+    
     return(
     <>
+        <div className="status">{status}</div>
         <div className="Board-row">
             <Square value={squares[0]} onsquareClick={()=>handleClick(0)}/>
             <Square value={squares[1]} onsquareClick={()=>handleClick(1)}/>
